1e70287b8833dc4cf80e53b06b8a9198d5c44a7f
sriharsha557 <sriharsha87@gmail.com>
2025-11-18 22:34:11 +0530

    Add missing captains (14HB and MXL5) to captain SQL script

--- sql/update_captains.sql
+++ sql/update_captains.sql
@@ -9,12 +9,14 @@ ALTER TABLE players ADD COLUMN IF NOT EXISTS is_vice_captain BOOLEAN DEFAULT FAL
 UPDATE players SET is_captain = FALSE, is_vice_captain = FALSE;
 
 -- Mark captains
-UPDATE players SET is_captain = TRUE WHERE player_id = '3H3Y';
-UPDATE players SET is_captain = TRUE WHERE player_id = '2BJB';
-UPDATE players SET is_captain = TRUE WHERE player_id = '615R';
-UPDATE players SET is_captain = TRUE WHERE player_id = '628B';
-UPDATE players SET is_captain = TRUE WHERE player_id = '53JX';
-UPDATE players SET is_captain = TRUE WHERE player_id = '7B8V';
+UPDATE players SET is_captain = TRUE WHERE player_id = '14HB';  -- Mahethar Reddy Bhavanam (Team 1)
+UPDATE players SET is_captain = TRUE WHERE player_id = 'MXL5';  -- Anil Mardani (Team 2)
+UPDATE players SET is_captain = TRUE WHERE player_id = '3H3Y';  -- Surya Kiran Aravelli (Team 3)
+UPDATE players SET is_captain = TRUE WHERE player_id = '2BJB';  -- Pradeep Kumar Pala (Team 4)
+UPDATE players SET is_captain = TRUE WHERE player_id = '615R';  -- Siva Teja Pidikiti (Team 5)
+UPDATE players SET is_captain = TRUE WHERE player_id = '628B';  -- Jagadish Babu Koppula Venkata (Team 6)
+UPDATE players SET is_captain = TRUE WHERE player_id = '53JX';  -- Sairam Singh Balaji (Team 7)
+UPDATE players SET is_captain = TRUE WHERE player_id = '7B8V';  -- Saravanan Krishnamoorthy (Team 8)
 
 -- Mark vice-captains
 UPDATE players SET is_vice_captain = TRUE WHERE player_id = '4V2H';
@@ -27,8 +29,10 @@ UPDATE players SET is_vice_captain = TRUE WHERE player_id = '6NN4';
 UPDATE players SET is_vice_captain = TRUE WHERE player_id = '646D';
 
 -- Assign captains and vice-captains to teams (pre-sold, excluded from auction)
-UPDATE players SET status = 'Sold', sold_to = 'Team 1', sold_price = 50 WHERE player_id = '4V2H';
-UPDATE players SET status = 'Sold', sold_to = 'Team 2', sold_price = 45 WHERE player_id = '4FF3';
+UPDATE players SET status = 'Sold', sold_to = 'Team 1', sold_price = 45 WHERE player_id = '14HB';  -- Captain: Mahethar
+UPDATE players SET status = 'Sold', sold_to = 'Team 1', sold_price = 50 WHERE player_id = '4V2H';  -- Vice-Captain: Debargha
+UPDATE players SET status = 'Sold', sold_to = 'Team 2', sold_price = 45 WHERE player_id = 'MXL5';  -- Captain: Anil
+UPDATE players SET status = 'Sold', sold_to = 'Team 2', sold_price = 45 WHERE player_id = '4FF3';  -- Vice-Captain: Dinesh
 UPDATE players SET status = 'Sold', sold_to = 'Team 3', sold_price = 50 WHERE player_id = '3H3Y';
 UPDATE players SET status = 'Sold', sold_to = 'Team 3', sold_price = 50 WHERE player_id = '5CLA';
 UPDATE players SET status = 'Sold', sold_to = 'Team 4', sold_price = 50 WHERE player_id = '2BJB';
